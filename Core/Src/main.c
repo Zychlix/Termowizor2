6cbfea9e0db5e9d6bdca33f10fb219efc6833811
Zychlix <mzflighter@gmail.com>
2025-02-11 19:55:22 +0100

    discooo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -108,9 +108,8 @@ const uint32_t zajepaleta [256] = {3150395,3282243,3348554,3414865,3481176,35474
 void wyswietl(uint32_t *paleta,uint8_t sposob, uint32_t * surowy)
 {
     volatile uint32_t *wekran=(uint32_t*)0xD0000000;
-
-    uint8_t luthist[16384]={0};
-    uint32_t histogram[0xffff] = {0};
+   uint8_t luthist[16384]={0};
+    uint32_t histogram[20000] = {0};
     //		uint32_t histogramz[16384] = {0};
 
     uint32_t chwilowa=0;
@@ -122,32 +121,30 @@ void wyswietl(uint32_t *paleta,uint8_t sposob, uint32_t * surowy)
     uint32_t ostatniaadr=0;
 
     uint32_t gorka=0;
-    uint32_t min=16384;
+    uint32_t min=100000;
     uint32_t max=0;
     uint8_t f2=0;
 
-    uint8_t flaga = 1;
 
 
 
 
 
-    if(flaga==1)
-    {
-        flaga=0;
-        for(uint32_t i=0; i<80000; i++)
+
+        for(uint32_t i=20000; i<80000; i++)
         {
-            histogram[(*(surowy+i))&0xffff]+=1;
-            srednia+=((*(surowy+i))&0xffff);
-            if((*(surowy+i))<min)
-                min=*(surowy+i);
-
+      //      histogram[(*(surowy+i))&0xffff]+=1;
+    //        srednia+=((*(surowy+i))&0xffff);
+            if(((*(surowy+i))&0x3fff)<min && ((*(surowy+i))&0x3fff)>2000)
+                min=(*(surowy+i)&0x3fff);
+            if(((*(surowy+i))&0x3fff)>max)
+                max=(*(surowy+i)&0x3fff);
         }
 
-        srednia=srednia/80000;
-        srednia+=1;
+       // srednia=srednia/80000;
+      //  srednia+=1;
 
-        for(uint32_t i=1; i<16384; i++)
+   /*     for(uint32_t i=1; i<16384; i++)
         {
             if(histogram[i]>300)
             {
@@ -210,7 +207,7 @@ void wyswietl(uint32_t *paleta,uint8_t sposob, uint32_t * surowy)
         //max+=100;
 
         int16_t kolor=0;
-        for(uint16_t i=0; i<250; i++)
+  */      for(uint16_t i=0; i<250; i++)
         {
             for(uint16_t x=0; x<325; x++)
             {
@@ -220,18 +217,18 @@ void wyswietl(uint32_t *paleta,uint8_t sposob, uint32_t * surowy)
                 //*(uint32_t*)(dest + 4 * pixel + 480 * lines*4)=((cam_buffer_2[pixel + 325*lines])<<8)|0xFF;
                 {
                     case 0:
-                        *(uint32_t*)(wekran + x + 480 * i)= paleta[luthist[(surowy[x+325*i])&0xffff]];//((R<<16)+(G<<8)+B)|0xFF000000;
+    //                    *(uint32_t*)(wekran + x + 480 * i)= paleta[luthist[(surowy[x+325*i])&0xffff]];//((R<<16)+(G<<8)+B)|0xFF000000;
                         break;
                     case 1:
-                        kolor=250*(((surowy[x+325*i])&0xffff)-niezeradr)/(ostatniaadr-niezeradr);
-                        if (kolor<1) kolor=0;
-                        if(kolor>254) kolor=255;
+         //               kolor=250*(((surowy[x+325*i])&0xffff)-niezeradr)/(ostatniaadr-niezeradr);
+         //               if (kolor<1) kolor=0;
+         //               if(kolor>254) kolor=255;
 //                        *(uint32_t*)(wekran + 4 * x + 480 * i*4)= paleta[kolor]|0xFF000000;
 
                         //if(buffer_index != 0)
                         {
-//                            *(uint32_t*)(wekran + x + 480 * i)= (surowy[x+325*i]<<8);
-                            *(uint32_t*)(wekran )= paleta[kolor];
+//*(uint32_t*)(wekran )= paleta[(surowy[x+325*i]-min)];
+*(uint32_t*)(wekran )= paleta[(255*((*(surowy+x+325*i))-min)/(max-min))%255];
                         }
                         break;
                 }
@@ -262,7 +259,7 @@ void wyswietl(uint32_t *paleta,uint8_t sposob, uint32_t * surowy)
 
 
     }
-}
+//}
 
 /* USER CODE END PV */
 
